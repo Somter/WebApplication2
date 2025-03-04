97c6364773f19be79c8a96467f8ce7101e58170e
Sasha <alecsand.unit@gmail.com>
2025-03-04 05:00:26 +0200

    Add project files.

---
+++ WebApplication2/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }  
+        public string Name { get; set; }
+        public string Director { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+        public string Poster { get; set; }
+        public string Description {  get; set; }
+    }
+}
---
+++ WebApplication2/Program.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+string? connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<MovieContext>(options => options.UseSqlServer(connection));
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Movie}/{action=Index}/{id?}");
+
+app.Run();
